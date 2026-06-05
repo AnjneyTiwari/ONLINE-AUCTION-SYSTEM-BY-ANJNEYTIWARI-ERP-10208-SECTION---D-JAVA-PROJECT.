49c416ce9639cd240eaea2c6336b93b20baf0ea8
AnjneyTiwari <anjney.tiwari@rungta.org>
2026-06-05 20:17:50 +0530

    Update Bidder.java

--- Bidder.java
+++ Bidder.java
@@ -1,16 +1,43 @@
-
 public class Bidder {
 
-    int bidderId;
-    String bidderName;
-
+    private int bidderId;
+    private String bidderName;
+    private double bidAmount;
 
     public Bidder(int bidderId, String bidderName) {
         this.bidderId = bidderId;
         this.bidderName = bidderName;
+        this.bidAmount = 0;
     }
 
-    public void placeBid(Item item, double bidAmount) {
-        item.updateBid(bidderName, bidAmount);
+    public int getBidderId() {
+        return bidderId;
+    }
+
+    public String getBidderName() {
+        return bidderName;
+    }
+
+    public double getBidAmount() {
+        return bidAmount;
+    }
+
+    public void placeBid(Item item, double amount) {
+
+        if (amount > item.getHighestBid()) {
+            item.setHighestBid(amount);
+            bidAmount = amount;
+
+            System.out.println("\nBid Accepted Successfully!");
+        } else {
+            System.out.println("\nBid Rejected!");
+            System.out.println("Bid must be higher than current highest bid.");
+        }
+    }
+
+    public void displayBidder() {
+        System.out.println("Bidder ID   : " + bidderId);
+        System.out.println("Bidder Name : " + bidderName);
+        System.out.println("Bid Amount  : ₹" + bidAmount);
     }
 }
